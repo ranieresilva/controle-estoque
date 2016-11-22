9bde895549b9f26f3c2661b891c07f580795cc34
Raniere Silva <me@ranieresilva.com>
2016-11-22 15:16:05 -0200

    versão do sistema contruído até a aula 7

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControleEstoque.Web.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,17 @@ namespace ControleEstoque.Web.Controllers
 {
     public class CadastroController : Controller
     {
+        private static List<GrupoProdutoModel> _listaGrupoProduto = new List<GrupoProdutoModel>()
+        {
+            new GrupoProdutoModel() { Id=1, Nome="Livros", Ativo=true },
+            new GrupoProdutoModel() { Id=2, Nome="Mouses", Ativo=true },
+            new GrupoProdutoModel() { Id=3, Nome="Monitores", Ativo=false }
+        };
+
         [Authorize]
         public ActionResult GrupoProduto()
         {
-            return View();
+            return View(_listaGrupoProduto);
         }
 
         [Authorize]
---
+++ ControleEstoque.Web/Models/GrupoProdutoModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleEstoque.Web.Models
+{
+    public class GrupoProdutoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
