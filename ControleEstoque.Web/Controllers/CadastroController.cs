ccd1088c1f0de1871421ee6c0377e6749afb5b51
Raniere Silva <me@ranieresilva.com>
2016-12-08 11:34:17 -0200

    versão do sistema contruído até a aula 9

--- ControleEstoque.Web/Controllers/CadastroController.cs
+++ ControleEstoque.Web/Controllers/CadastroController.cs
@@ -29,6 +29,43 @@ namespace ControleEstoque.Web.Controllers
             return Json(_listaGrupoProduto.Find(x => x.Id == id));
         }
 
+        [HttpPost]
+        [Authorize]
+        public ActionResult ExcluirGrupoProduto(int id)
+        {
+            var ret = false;
+
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == id);
+            if (registroBD != null)
+            {
+                _listaGrupoProduto.Remove(registroBD);
+                ret = true;
+            }
+
+            return Json(ret);
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult SalvarGrupoProduto(GrupoProdutoModel model)
+        {
+            var registroBD = _listaGrupoProduto.Find(x => x.Id == model.Id);
+
+            if (registroBD == null)
+            {
+                registroBD = model;
+                registroBD.Id = _listaGrupoProduto.Max(x => x.Id) + 1;
+                _listaGrupoProduto.Add(registroBD);
+            }
+            else
+            {
+                registroBD.Nome = model.Nome;
+                registroBD.Ativo = model.Ativo;
+            }
+
+            return Json(registroBD);
+        }
+
         [Authorize]
         public ActionResult MarcaProduto()
         {
