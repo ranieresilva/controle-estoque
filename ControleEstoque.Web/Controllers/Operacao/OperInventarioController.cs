cd5a2644ec2947f011403265bb11c5760ffeb024
Raniere Silva <me@ranieresilva.com>
2018-06-16 09:25:39 -0300

    versão do sistema contruído até a aula 46.

---
+++ ControleEstoque.Web/Controllers/Operacao/OperInventarioController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    public class OperInventarioController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
