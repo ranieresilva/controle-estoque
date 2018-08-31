ddddd556b895c8e1a9a1a04096789546f10e4e39
Raniere Silva <me@ranieresilva.com>
2018-08-31 10:17:57 -0300

    versão do sistema contruído até a aula 56.

---
+++ ControleEstoque.Web/Controllers/Operacao/OperLancamentoPerdaProdutoController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace ControleEstoque.Web.Controllers
+{
+    [Authorize(Roles = "Gerente,Administrativo")]
+    public class OperLancamentoPerdaProdutoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
