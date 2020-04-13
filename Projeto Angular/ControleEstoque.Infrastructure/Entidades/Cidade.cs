c3ea01ba1e73d9d93cad46fe3e8e7f58097046a3
Lester Cerioli <admin@lucastecnologia.onmicrosoft.com>
2020-04-12 21:32:41 -0300

    Configuração das Entidades

---
+++ Projeto Angular/ControleEstoque.Infrastructure/Entidades/Cidade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleEstoque.Domain.Entidades
+{
+    public class Cidade : Entidade
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public bool Ativo { get; set; }
+        public int EstadoId { get; set; }
+
+
+        public override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Projeto Angular/ControleEstoque.Infrastructure/Entidades/Pais.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ControleEstoque.Domain.Entidades
+{
+    public class Pais : Entidade
+    {
+
+
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+        public bool Ativo { get; set; }
+
+        public override void Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
